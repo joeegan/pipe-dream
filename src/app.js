424d5be9396927314b51865b246b242b42fe38f8
Joe Egan <joeegan84@gmail.com>
2016-12-20 22:38:53 +0000

    drag n drop groundwork

--- src/app.js
+++ src/app.js
@@ -1,47 +1,83 @@
 import React, { Component, PropTypes } from 'react';
+import Grid from './components/grid'
+import Tile from './components/tile'
 import _ from 'lodash';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
 const zeroArray = (n) => new Array(n).fill(0);
-const grid = zeroArray(7).map(n => zeroArray(10));
+
+let position = [0, 0];
+let observer = null;
+
+function emitChange() {
+  observer(position);
+}
+
+export function observe(o) {
+  if (observer) {
+    throw new Error('Multiple observers not implemented.');
+  }
+
+  observer = o;
+  emitChange();
+
+  return () => {
+    observer = null;
+  };
+}
+
+export function move(toX, toY) {
+  position = [toX, toY];
+  emitChange();
+}
+
+export function canMove(toX, toY) {
+  return true;
+}
 
 class App extends Component {
 
   constructor(props: object) {
     super(props);
+    this.unobserve = observe(this.handleChange.bind(this));
+  }
+
+  componentWillUnmount() {
+    this.unobserve();
+  }
+
+  handleChange(position) {
+    const nextState = { position };
+    if (this.state) {
+      this.setState(nextState);
+    } else {
+      this.state = nextState;
+    }
   }
 
   get controls() {
     return zeroArray(5).map((cell, i) => {
       return (
         <div key={i} className='cell'>
-          <div className='tile'>
-            <span className='numero'>{i}</span>
-          </div>
+          <Tile
+            id={i}
+            handleChange={this.handleChange.bind(this)}
+          />
         </div>
       );
     });
   }
 
-  get grid() {
-    return grid.map(row => {
-      return (
-        <div className='row'>{row.map((r, i) => {
-          return (
-            <div key={i} className='cell'></div>
-          );
-        })}</div>
-      );
-    });
-  }
-
   render() {
+    const { position } = this.state;
     return (
       <div>
         <div className='controls'>{this.controls}</div>
-        <div className='grid'>{this.grid}</div>
+        <Grid tilePosition={this.props.tilePosition} />
       </div>
     )
   }
 }
 
-export default App;
+export default DragDropContext(HTML5Backend)(App);
