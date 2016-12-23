662f3cd53fadfe55ffe24bef190ecd719b6e6b27
Joe Egan <joeegan84@gmail.com>
2016-12-23 21:02:12 +0000

    remove befores from css

--- src/components/tile.js
+++ src/components/tile.js
@@ -4,15 +4,19 @@ import _ from 'lodash';
 class Tile extends Component {
 
   render() {
+    const pipeClassName= `pipe ${this.props.type}`
     return (
-      <div className='tile'>
-        <span className={this.props.type}
-              style={{
+      <div className='tile'
+           style={{
                 opacity: this.props.hovered ? 0.4 : 1,
                 background: this.props.hovered ? 'blue' : 'lightgreen',
                 zIndex: this.props.hovered ? 1 : 0,
               }}
-        ></span>
+      >
+        <span className={pipeClassName} />
+        {this.props.type == 'CROSS' &&
+          <span className={pipeClassName} />
+        }
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './app';
 import app from './styles/app.css';
+import pipe from './styles/pipe.css';
 
 render(<App />, document.querySelector('#root'));
